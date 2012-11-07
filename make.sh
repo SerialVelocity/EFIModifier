31f3bfb19858d1df47a636336a7b7e14d41f07be
Ben Grabham <bsg110@ic.ac.uk>
2012-11-07 18:37:45 +0000

    Switched from full EFI toolkit to slimmed down patch tool files

--- make.sh
+++ make.sh
@@ -9,15 +9,12 @@ die () {
 git submodule init
 git submodule update
 
-cd BaseTools
-make all
-[[ "$?" -eq 0 ]] || die "Unable to make the library"
-cd ..
-
 #Release
-dmd -release -property -w -nofloat -noboundscheck main.d EFI.d Console.d Utils.d ../../../buildtools-BaseTools/Source/C/libs/libCommon.a -ofmain.release
+gcc -O2 -c PMPatch/Tiano/TianoDecompress.c -o TianoDecompress.o
+dmd -release -property -w -nofloat -noboundscheck main.d EFI.d Console.d Utils.d TianoDecompress.o -ofmain.release
 [[ "$?" -eq 0 ]] || die "Unable to make release version"
 
 #Debug
-dmd -debug -gc -property -w main.d EFI.d Console.d Utils.d ../../../buildtools-BaseTools/Source/C/libs/libCommon.a -ofmain.debug
+gcc -g -c PMPatch/Tiano/TianoDecompress.c -o TianoDecompress.o
+dmd -debug -gc -property -w main.d EFI.d Console.d Utils.d TianoDecompress.o -ofmain.debug
 [[ "$?" -eq 0 ]] || die "Unable to make debug version"
